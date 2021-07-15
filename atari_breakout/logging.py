5c2f5ff849cae8248598ef85dcecd8b4d5bb348e
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-07-15 10:49:17 -0500

    Logging improvements

--- atari_breakout/logging.py
+++ atari_breakout/logging.py
@@ -1,10 +1,41 @@
 import logging
 import os
 import sys
-from logging import handlers
+from logging import Logger, handlers
 from pathlib import Path
+from typing import Any
 
 import coloredlogs
+
+
+class StreamToLogger:
+    """Fake file-like stream object that redirects writes to a logger instance."""
+
+    def __init__(self, logger: Logger, log_level: int = logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf: str = ""
+
+    def write(self, buf: Any) -> None:
+        """Write a line to the stream."""
+        temp_linebuf = self.linebuf + buf
+        self.linebuf = ""
+        for line in temp_linebuf.splitlines(True):
+            # From the io.TextIOWrapper docs:
+            #   On output, if newline is None, any '\n' characters written
+            #   are translated to the system default line separator.
+            # By default sys.stdout.write() expects '\n' newlines and then
+            # translates them so this is still cross platform.
+            if line[-1] == "\n":
+                self.logger.log(self.log_level, line.rstrip())
+            else:
+                self.linebuf += line
+
+    def flush(self) -> None:
+        """Output the log."""
+        if self.linebuf != "":
+            self.logger.log(self.log_level, self.linebuf.rstrip())
+        self.linebuf = ""
 
 
 def setup() -> None:
@@ -37,6 +68,12 @@ def setup() -> None:
     if "COLOREDLOGS_LOG_LEVEL" not in os.environ:
         coloredlogs.DEFAULT_LOG_LEVEL = log_level
 
-    coloredlogs.install(logger=root_log, stream=sys.stdout)
+    coloredlogs.install(logger=root_log, stream=sys.__stdout__)
 
     logging.getLogger("pygame").setLevel(logging.WARNING)
+
+    stdout_logger = logging.getLogger("STDOUT")
+    sys.stdout = StreamToLogger(stdout_logger, logging.INFO)
+
+    stderr_logger = logging.getLogger("STDERR")
+    sys.stderr = StreamToLogger(stderr_logger, logging.ERROR)

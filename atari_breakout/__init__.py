ee27ea5330f0fdc01468ad2456b96dbef8c40e4a
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-07-15 19:29:39 -0500

    Setup python-dotenv

--- atari_breakout/__init__.py
+++ atari_breakout/__init__.py
@@ -1,5 +1,16 @@
+import logging as py_logging
+
 from . import logging
 
 __version__ = "0.1.0"
 
 logging.setup()
+log = py_logging.getLogger("atari_breakout.init")
+
+try:
+    from dotenv import load_dotenv
+
+    log.info("Found .env file, loading environment variables from it.")
+    load_dotenv(override=True)
+except ModuleNotFoundError:
+    log.warning("No python-dotenv, not loading .env file.")
